9af6b1f1fa441cc2ebb458ed2bc2d631eb8c6a60
digglife <digglife@gmail.com>
2015-06-26 23:46:17 +0800

    reschedule_wait_time can be disabled

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,37 +4,70 @@
 #
 # === Parameters
 #
-# Document parameters here.
-#
 # [*enabled*]
-#   (boolean) Enable automatic update or not.
+#   (boolean) Enable automatic update or not. Default is true.
 # [*type*]
-#   (string) options for automatic update. Possible types are:
-#    1. `notify`. 
-#    2. `download`.
-#    3. `schedule`.
-#    4. `custom`.
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+#   (string) options for automatic update. Default is `schedule`.
+#    Possible types are:
+#    1. `notify`. Notify before download.
+#    2. `download`. Automatically download and notify of installation. 
+#    3. `schedule`. Automatically download and schedule installation.
+#    4. `custom`. Automatic Updates is required, but end users can configure it.
+# [*wsus_server*]
+#   (string) URL of WSUS Server. Default is `undef`.
+# [*target_group*]
+#   (string) target group on WSUS Server. Default is `undef`.
+# [*auto_reboot*]
+#   (boolean) 
+#   **true**  : Notifies user that the server will restart in 15 minutes.
+#   **false** : Logged-on user can choose whether or not to restart.
+#   The server will restart anyway if there is no logged-on user. 
+#   Default is `true`.
+# [*auto_install_minor_updates*]
+#   (boolean) Silently install minor updates(no restart required) or not. 
+#   Default is `false`.
+# [*include_recommended_updates*]
+#   (boolean) install recommended updates or not. Default is `false`.
+# [*power_management_enabled*]
+#   (boolean) whether wake up the server to install the updates. Default is `true`.
+# [*scheduled_install_day*]
+#   (integer) `0` for `daily`. `1-7` for `Sunday(1)` to `Saturday(7)`. Default is `7`.
+#   Only applies when `type` is `schedule`.
+# [*scheduled_install_time*]
+#   (integer) 24-hour format (`0–23`). Default is `3`.
+#   Only applies when `type` is `schedule`.
+# [*detection_frequency*]
+#   (integer) Time in hours (`1–22`) between detection cycle. Default is `22`.
+# [*reboot_relaunch_timeout*]
+#   (integer) Time in minutes(`1-1440`) between prompting again for a scheduled restart.
+#   Default is `10`.
+# [*reboot_warning_timeout*]
+#   (integer) Time in minutes (`1-30`) of the restart warning countdown,
+#   after installing updates with a deadline or scheduled updates.
+#   Default is `5`.
+# [*reschedule_wait_time*]
+#   (integer) Time in minutes(`0-60`), that Automatic Updates should wait at startup 
+#   before applying updates from a missed scheduled installation time.
+#   This policy applies only when `type` is `schedule`.
+#   Default is `1`.
+#   *NOTE* : if it's set as 0,  a missed scheduled installation will occur
+#            during the next scheduled installation time.
 #
 # === Examples
 #
-#  class { 'wuau':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+# class { 'wuau':
+#     enabled      => true,
+#     type         => 'schedule',
+#     wsus_server  => 'http://10.0.0.111',
+#     target_group => 'Production',
+#     auto_reboot  => false,
+# }
+#
+
 #
 # === Authors
 #
-# Zhu Sheng Li <digglife@gmail.com>
+# Eric Zhu(朱聖黎) <digglife@gmail.com>
 #
 # === References
 #
@@ -43,14 +76,14 @@
 #
 # === Copyright
 #
-# Copyright 2015 Zhu Sheng Li, unless otherwise noted.
+# Copyright 2015 Eric Zhu(朱聖黎) <digglife@gmail.com>, unless otherwise noted.
 #
 class wuau(
   $enabled = true,
   $type = 'schedule',
   $wsus_server = undef,
   $target_group = undef,
-  $auto_reboot = false,
+  $auto_reboot = true,
   $auto_install_minor_updates = false,
   $include_recommended_updates = false,
   $power_management_enabled = true,
@@ -81,11 +114,9 @@ class wuau(
   # Default 10(minutes) if disabled or not configured.
   validate_integer($reboot_relaunch_timeout, 1440, 1)
   # Default 5(miniutes) if disabled or not configured.
-  # Note: when I tested on Windows Server 2008, the default value is 15mins,
-  #       which is different from the official documents.
   validate_integer($reboot_warning_timeout, 40, 1)
-  # Default 1(minute) if disabled or not configured.
-  validate_integer($reschedule_wait_time, 60, 1)
+  # Default 1(minute) if not configured.
+  validate_integer($reschedule_wait_time, 60, 0)
 
   # convert boolean value to integer for writting into registry.
   $NoAutoUpdate = bool2num(!$enabled)
@@ -93,6 +124,8 @@ class wuau(
   $AutoInstallMinorUpdates = bool2num($auto_install_minor_updates)
   $IncludeRecommendedUpdates = bool2num($include_recommended_updates)
   $AUPowerManagement = bool2num($power_management_enabled)
+  # if $reschedule_wait_time is 0 then 0, otherwise 1.
+  $RescheduleWaitTimeEnabled = bool2num(num2bool($reschedule_wait_time))
 
   $AUOptions = $type ? {
     'notify'    => 2,
@@ -120,9 +153,9 @@ class wuau(
     ]
   }
 
-  registry_key { $au_reg_path:
+  registry_key { 'AutomaticUpdate':
+    path         => $au_reg_path,
     ensure       => present,
-    purge_values => true,
   }
 
   if $wsus_server {
@@ -132,7 +165,7 @@ class wuau(
       ensure => present,
       path   => "$au_reg_path\\UseWUServer",
       type   => 'dword',
-      data   => '1',
+      data   => 1,
       notify => Service['wuauserv'],
     }
 
@@ -171,6 +204,18 @@ class wuau(
         notify => Service['wuauserv']
       }
     }
+
+  } else {
+
+    registry_value { 'UseWUServer':
+      # or use ensure => absent ?
+      ensure => present,
+      path   => "$au_reg_path\\UseWUServer",
+      type   => 'dword',
+      data   => 0,
+      notify => Service['wuauserv'],
+    }
+
   }
 
 
@@ -277,12 +322,11 @@ class wuau(
     data   => $reboot_warning_timeout,
   }
 
-
   registry_value { 'RescheduleWaitTimeEnabled':
     ensure => present,
     path   => "$au_reg_path\\RescheduleWaitTimeEnabled",
     type   => 'dword',
-    data   => 1,
+    data   => $RescheduleWaitTimeEnabled,
   }
 
   registry_value { 'RescheduleWaitTime':
@@ -291,6 +335,5 @@ class wuau(
     type   => 'dword',
     data   => $reschedule_wait_time,
   }
-
 
 }
